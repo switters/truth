fd3fd78c0aba5aba4b26dd33ab1cdc67a3dff5e8
switters <codenomadic@gmail.com>
2017-05-03 09:50:55 -0700

    Truth up.

---
+++ truth.js
@@ -0,0 +1,25 @@
+/**
+ * Tiny env var resolver
+ *
+ * !!! PRECEDENCE MATTERS !!!
+ *
+ * example (from lowest priority to highest):
+ *     .env
+ *     < ```COMMAND_LINE_DECLARED node server.js```
+ *     < ```node server.js --command_line_declared``` (case-insensitive)
+ */
+
+// precedence 0 (defaults)
+const dotenv = require('dotenv').config().parsed;
+
+// precedence 1 (COMMAND_LINE_DECLARED=true node server.js)
+const node_env = process.env;
+
+// precedence Infinity (node server.js --command_line_declared true)
+const argv = require('yargs').argv;
+// add upper-cased versions of command-line-supplied arguments
+const argv_uc = Object.keys(argv).forEach(k => argv[k.toUpperCase()] = argv[k]);
+
+// export a new object
+module.exports = Object.assign({}, dotenv, node_env, argv);
+
